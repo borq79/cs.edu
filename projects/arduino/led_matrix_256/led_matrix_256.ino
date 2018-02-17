b0503fa41396447ba07efe85b0d21d3a62a6a182
ryanebrock <rbrock@zebra.com>
2018-02-17 09:44:58 -0600

    Lower case seems easier to read, but to avoid conflicts with the struct I renamed the struct values. I left the dark pixels upper case as that seemed to enhance the clarity

--- projects/arduino/led_matrix_256/led_matrix_256.ino
+++ projects/arduino/led_matrix_256/led_matrix_256.ino
@@ -17,74 +17,74 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_OF_PIXELS, MATRIX_DATA_PIN, NEO_
 //int sensorValue = 0;  // variable to store the value coming from the sensor
 
 struct COLOR {
-  byte r;
-  byte g;
-  byte b;
+  byte red;
+  byte green;
+  byte blue;
 };
 
 #define D {0x00, 0x00, 0x00}
-#define B {0x00, 0x00, 0xFF}
-#define G {0x00, 0xFF, 0x00}
-#define R {0xFF, 0x00, 0x00}
-#define P {0xFF, 0x00, 0xFF}
-#define Y {0xFF, 0xFF, 0x00}
-#define A {0x00, 0xFF, 0xFF}
-#define W {0xFF, 0xFF, 0xFF}
+#define b {0x00, 0x00, 0xFF}
+#define g {0x00, 0xFF, 0x00}
+#define r {0xFF, 0x00, 0x00}
+#define p {0xFF, 0x00, 0xFF}
+#define y {0xFF, 0xFF, 0x00}
+#define a {0x00, 0xFF, 0xFF}
+#define w {0xFF, 0xFF, 0xFF}
 
 
 const PROGMEM COLOR frames[][NUM_OF_PIXELS] = { {
-        D,D,W,W,D,D,D,D,D,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,D,D,D,D,W,D,W,D,D,
-        D,D,D,W,D,W,D,D,D,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,D,D,W,D,D,D,D,D,D,
-        D,D,D,D,D,D,W,W,W,W,W,D,D,D,D,D,
-        D,D,D,D,D,W,W,W,W,W,D,D,D,D,D,D,
-        D,D,D,D,D,R,R,R,G,R,R,R,D,D,D,D,
-        D,D,D,R,R,R,R,G,R,R,R,R,D,D,D,D,
-        D,D,D,R,R,R,R,G,G,G,R,R,R,R,D,D,
-        D,D,R,R,R,R,G,G,G,R,R,R,R,D,D,D,
-        D,D,D,R,R,R,G,G,G,G,G,R,R,R,D,D,
-        D,D,R,R,G,G,G,G,G,G,G,R,R,D,D,D,
-        D,D,D,R,G,G,G,G,G,G,G,G,G,R,D,D,
-        D,D,D,R,R,R,R,Y,R,R,R,R,D,D,D,D,
-        D,D,D,D,D,R,R,R,Y,R,R,R,D,D,D,D,
-        D,D,D,D,D,R,R,Y,R,R,D,D,D,D,D,D
+        D,D,w,w,D,D,D,D,D,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,D,D,D,D,w,D,w,D,D,
+        D,D,D,w,D,w,D,D,D,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,D,D,w,D,D,D,D,D,D,
+        D,D,D,D,D,D,w,w,w,w,w,D,D,D,D,D,
+        D,D,D,D,D,w,w,w,w,w,D,D,D,D,D,D,
+        D,D,D,D,D,r,r,r,g,r,r,r,D,D,D,D,
+        D,D,D,r,r,r,r,g,r,r,r,r,D,D,D,D,
+        D,D,D,r,r,r,r,g,g,g,r,r,r,r,D,D,
+        D,D,r,r,r,r,g,g,g,r,r,r,r,D,D,D,
+        D,D,D,r,r,r,g,g,g,g,g,r,r,r,D,D,
+        D,D,r,r,g,g,g,g,g,g,g,r,r,D,D,D,
+        D,D,D,r,g,g,g,g,g,g,g,g,g,r,D,D,
+        D,D,D,r,r,r,r,y,r,r,r,r,D,D,D,D,
+        D,D,D,D,D,r,r,r,y,r,r,r,D,D,D,D,
+        D,D,D,D,D,r,r,y,r,r,D,D,D,D,D,D
     },
 
-    {   D,D,D,D,D,D,D,D,R,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,R,D,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,R,W,R,D,D,D,D,D,D,
-        D,D,D,D,D,R,R,W,R,R,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,R,R,R,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,Y,D,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,D,Y,D,D,D,D,D,D,D,
-        D,D,D,D,W,W,W,W,W,W,W,D,D,D,D,D,
-        D,D,D,D,D,W,W,W,W,W,W,W,D,D,D,D,
-        D,D,D,D,W,W,W,W,W,W,W,D,D,D,D,D,
-        D,D,D,D,D,W,W,W,W,W,W,W,D,D,Y,D,
-        Y,D,Y,D,W,W,W,W,W,W,W,D,D,D,D,D,
-        Y,Y,Y,D,D,W,W,W,W,W,W,W,D,D,Y,D,
-        D,Y,Y,Y,W,W,W,W,W,W,W,Y,Y,Y,Y,D,
-        D,D,D,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,D,D,
-        D,D,D,Y,Y,Y,Y,Y,Y,Y,Y,Y,D,D,D,D
+    {   D,D,D,D,D,D,D,D,r,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,r,D,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,r,w,r,D,D,D,D,D,D,
+        D,D,D,D,D,r,r,w,r,r,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,r,r,r,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,y,D,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,D,y,D,D,D,D,D,D,D,
+        D,D,D,D,w,w,w,w,w,w,w,D,D,D,D,D,
+        D,D,D,D,D,w,w,w,w,w,w,w,D,D,D,D,
+        D,D,D,D,w,w,w,w,w,w,w,D,D,D,D,D,
+        D,D,D,D,D,w,w,w,w,w,w,w,D,D,y,D,
+        y,D,y,D,w,w,w,w,w,w,w,D,D,D,D,D,
+        y,y,y,D,D,w,w,w,w,w,w,w,D,D,y,D,
+        D,y,y,y,w,w,w,w,w,w,w,y,y,y,y,D,
+        D,D,D,y,y,y,y,y,y,y,y,y,y,y,D,D,
+        D,D,D,y,y,y,y,y,y,y,y,y,D,D,D,D
     },
 
-    {   D,D,D,D,D,D,D,G,G,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,D,G,G,D,D,D,D,D,D,D,
-        D,D,D,D,D,D,B,G,G,G,D,D,D,D,D,D,
-        D,D,D,D,D,D,G,G,B,R,D,D,D,D,D,D,
-        D,D,D,D,D,G,G,G,B,G,B,D,D,D,D,D,
-        D,D,D,D,D,G,B,G,G,G,B,D,D,D,D,D,
-        D,D,D,D,G,G,B,G,G,G,B,B,D,D,D,D,
-        D,D,D,D,G,R,G,B,B,G,G,G,D,D,D,D,
-        D,D,D,G,B,G,G,G,G,B,B,G,G,D,D,D,
-        D,D,D,G,B,P,G,G,G,B,B,G,G,D,D,D,
-        D,D,B,G,P,G,G,B,G,G,G,G,B,B,D,D,
-        D,D,G,G,Y,G,B,B,G,R,G,B,B,G,D,D,
-        D,G,G,G,G,B,B,G,G,G,B,B,G,G,G,D,
-        D,D,D,D,D,D,Y,Y,Y,Y,D,D,D,D,D,D,
-        D,D,D,D,D,D,Y,Y,Y,Y,D,D,D,D,D,D,
-        D,D,D,D,D,D,Y,Y,Y,Y,D,D,D,D,D,D
+    {   D,D,D,D,D,D,D,g,g,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,D,g,g,D,D,D,D,D,D,D,
+        D,D,D,D,D,D,b,g,g,g,D,D,D,D,D,D,
+        D,D,D,D,D,D,g,g,b,r,D,D,D,D,D,D,
+        D,D,D,D,D,g,g,g,b,g,b,D,D,D,D,D,
+        D,D,D,D,D,g,b,g,g,g,b,D,D,D,D,D,
+        D,D,D,D,g,g,b,g,g,g,b,b,D,D,D,D,
+        D,D,D,D,g,r,g,b,b,g,g,g,D,D,D,D,
+        D,D,D,g,b,g,g,g,g,b,b,g,g,D,D,D,
+        D,D,D,g,b,p,g,g,g,b,b,g,g,D,D,D,
+        D,D,b,g,p,g,g,b,g,g,g,g,b,b,D,D,
+        D,D,g,g,y,g,b,b,g,r,g,b,b,g,D,D,
+        D,g,g,g,g,b,b,g,g,g,b,b,g,g,g,D,
+        D,D,D,D,D,D,y,y,y,y,D,D,D,D,D,D,
+        D,D,D,D,D,D,y,y,y,y,D,D,D,D,D,D,
+        D,D,D,D,D,D,y,y,y,y,D,D,D,D,D,D
     }
 };
 
@@ -122,7 +122,7 @@ void renderFrame( uint8_t frameNumber ) {
     // Setup every pixels
     for(uint8_t i = 0; i < strip.numPixels(); i++) {
         pixel = frames[frameNumber][i];
-        strip.setPixelColor(i, pixel.r, pixel.g, pixel.b);
+        strip.setPixelColor(i, pixel.red, pixel.green, pixel.blue);
     }
 
     // Render the frame
